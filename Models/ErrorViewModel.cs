9fe6b56ee67acc95d7806365c0b77726323f6efe
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-02-24 15:02:06 +0900

    add first project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appsrvkeyvault.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
